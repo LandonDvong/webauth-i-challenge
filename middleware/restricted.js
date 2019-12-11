e2e9715216b935edce326ff487cb581e096adb39
thaveesit douangaphaivong <d15.landon@gmail.com>
2019-12-10 20:54:24 -0600

    added session support

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,21 +1,25 @@
-const bcrypt = require("bcryptjs");
-const users = require("../routes/users-model.js");
+// const bcrypt = require("bcryptjs");
+// const users = require("../routes/users-model.js");
 
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
+//   const { username, password } = req.headers;
 
-  if (!username || !password)
-    res.status(401).json({ message: "You shall not pass!" });
-  else {
-    try {
-      const getUser = users.findUser(username);
-      if (!getUser) res.status(404).json({ message: "You shall not pass!" });
-      else if (getUser && bcrypt.compareSync(password, getUser.password))
-        next();
-    } catch (error) {
-      res.status(500).json({ error: "db error: ", error });
-    }
-  }
+//   if (!username || !password)
+//     res.status(401).json({ message: "You shall not pass!" });
+//   else {
+//     try {
+//       const getUser = users.findUser(username);
+//       if (!getUser) res.status(404).json({ message: "You shall not pass!" });
+//       else if (getUser && bcrypt.compareSync(password, getUser.password))
+//         next();
+//     } catch (error) {
+//       res.status(500).json({ error: "db error: ", error });
+//     }
+//   }
+// }
+req.session && req.session.user
+? next()
+: res.status(401).json({ message: "You shall not pass!" });
 }
 
 module.exports = restricted;
